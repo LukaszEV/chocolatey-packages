19270a2d7a41d16e4fb289ea25a06c5244346efb
Lukasz EV <lchwastyk@ecovadis.com>
2022-12-06 12:46:15 +0100

    upadting filebeat and winlogbeat

--- automatic/winlogbeat/tools/chocolateyinstall.ps1
+++ automatic/winlogbeat/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName= 'winlogbeat'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.15.1-windows-x86.zip'
-$url64      = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-7.15.1-windows-x86_64.zip'
+$url        = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-8.5.2-windows-x86_64.zip'
+$url64      = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-8.5.2-windows-x86_64.zip'
 
 $installationPath = $toolsDir
 
@@ -15,9 +15,9 @@ $packageArgs = @{
   unzipLocation = $installationPath
   url           = $url
   url64bit      = $url64
-  checksum      = '1284c2025284e1f2db0d738aae19636f65eee6985242622b65ea889dcb5d0e6af67b712f7c322ed863c2e317eda0dea6d88ae99b7b063f3d25d0dd6218e28c54'
+  checksum      = 'fc2fd0c1737712b8deee08a14a01dbcc5cf1acf11c1459a98352c0a8f00fc75885742bbf17b84e13d8a880aae4414a3dc844334261068758bdc7f77c52c67990'
   checksumType  = 'sha512'
-  checksum64    = 'd12038a3f1b23c23cfc9f7f1dd80be6c7ec313321d684bb20c50964bc2dcb5150658af3e5fc1f956b3d9489933d45ec72ca748122797e8eda053a8a87b943c78'
+  checksum64    = 'fc2fd0c1737712b8deee08a14a01dbcc5cf1acf11c1459a98352c0a8f00fc75885742bbf17b84e13d8a880aae4414a3dc844334261068758bdc7f77c52c67990'
   checksumType64= 'sha512'
   specificFolder = $folder
 }
