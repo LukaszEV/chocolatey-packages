2ee88fa721df97addf044e8ab5e7c2b53fdaabbe
Szymon Stawski <sstawski@ecovadis.com>
2023-09-01 13:12:41 +0200

    metricbeat package added and pushed

--- automatic/metricbeat/tools/chocolateyinstall.ps1
+++ automatic/metricbeat/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName= 'metricbeat'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.15.1-windows-x86.zip'
-$url64      = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.15.1-windows-x86_64.zip'
+$url        = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.5.2-windows-x86_64.zip'
+$url64      = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-8.5.2-windows-x86_64.zip'
 
 $installationPath = $toolsDir
 
@@ -15,9 +15,9 @@ $packageArgs = @{
   unzipLocation = $installationPath
   url           = $url
   url64bit      = $url64
-  checksum      = 'f7718c387de46bdf198ff04de7e2a67a8dcffd0950117b2e06212f803913f049d7635f57940830818b3a991c30e481eea7db814689d78eadbdf0bc1be8a4b4de'
+  checksum      = 'F65C9F127938070C2856FE1F21BC2D774B2CF465D36615B793C7D44D59561F8C7C312EF8F6E20C12CA9F1ECBA5EC56EE6C6F0FD48EBA64F42DDCE54FB9830356'
   checksumType  = 'sha512'
-  checksum64    = 'f9ac22e1ef52feecc4534b8589aba0cbbe82e36a65aaa90484e4e785c7520ea19ae781b5554eef926b2a9200d3a23360c7b5127f3768fa063ba1e45e136e9417'
+  checksum64    = 'F65C9F127938070C2856FE1F21BC2D774B2CF465D36615B793C7D44D59561F8C7C312EF8F6E20C12CA9F1ECBA5EC56EE6C6F0FD48EBA64F42DDCE54FB9830356'
   checksumType64= 'sha512'
   specificFolder = $folder
 }
